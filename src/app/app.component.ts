da831f0cab86f59043f8842c5dafb994f2f67a8c
AtouiIlyes <atoui.ilyes@gmail.com>
2017-08-21 16:58:25 +0100

    rendre le site dynamique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,12 @@ import {Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<router-outlet></router-outlet>'
+  template: `
+    <menu-list ></menu-list>
+    <hr>
+    <router-outlet ></router-outlet>
+
+    `
 
 })
 export class AppComponent {
